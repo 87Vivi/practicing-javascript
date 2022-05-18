c77ef14946467eba815d55e3ecb4bceebd9212a1
87vivi <viviana.castaldo@gmx.de>
2022-05-18 10:26:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//Return the sum of two numbers
+function addition(a, b) {
+  return a + b;
+}
+addition(2, 3);
+
+// Return the remainder of two numbers
+function remainder(x, y) {
+  return x % y;
+}
+remainder(3, 1);
+
+// Swap two given numbers
+
+function swap(a, b) {
+  return [b, a];
+}
+swap(10, 26);
+
+// Return the fist element in an array
+function getFirstValue(arr) {
+  return arr[0];
+}
+getFirstValue([1, 2, 5, 9]);
+
+// Return the next number from the integer passed
+function addition(num) {
+  return num + 1;
+}
+addition(5);
